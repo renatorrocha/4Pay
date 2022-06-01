a48f2cbb99042a0aa02c70702361aa42cb7db498
Gabriel Santos Farias <gfarias@server01.foursys.local>
2022-06-01 15:57:51 -0300

    Tela Pix transferencia

--- app/src/main/java/com/example/appbanco/MainActivity.java
+++ app/src/main/java/com/example/appbanco/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.appbanco;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
 
+        button = findViewById(R.id.btnEntrar);
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(), PixTransf.class));
+            }
+        });
+
+
+
+//        button.setOnClickListener(view1 -> {
+//            startActivity(new Intent(this, Cartoes.class));
+//        });
     }
 }
