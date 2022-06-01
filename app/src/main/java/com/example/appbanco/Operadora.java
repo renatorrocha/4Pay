2a8fbd379b6407d23c1c0670ba5d1c44a1f052d5
Renato <renato.rodrigues@foursys.com.br>
2022-06-01 11:16:25 -0300

    Xml Recarga navegacao entre telas

---
+++ app/src/main/java/com/example/appbanco/Operadora.java
@@ -0,0 +1,14 @@
+package com.example.appbanco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Operadora extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operadora);
+    }
+}
