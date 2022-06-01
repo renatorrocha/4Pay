d1e0c66a833cc9283f922a06bd12d4ea4056a2c3
Gabriel Santos Farias <gfarias@server01.foursys.local>
2022-06-01 11:57:29 -0300

    Telas de cartões

---
+++ app/src/main/java/com/example/appbanco/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.appbanco;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
+
+
+    }
+}
---
+++ app/src/main/java/com/example/appbanco/TipoConta.java
@@ -0,0 +1,14 @@
+package com.example.appbanco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TipoConta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipo_conta);
+    }
+}
