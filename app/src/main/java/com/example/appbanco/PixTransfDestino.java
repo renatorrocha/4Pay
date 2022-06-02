d26943cdbc7be13a761e8d90be07d0ba6dfb0902
Igor Montezuma de Miranda <imiranda@server01.foursys.local>
2022-06-02 10:08:42 -0300

    First Commmit

---
+++ app/src/main/java/com/example/appbanco/PixTransfDestino.java
@@ -0,0 +1,14 @@
+package com.example.appbanco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PixTransfDestino extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pix_transf_destino);
+    }
+}
