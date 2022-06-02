de9e6dcd9e81b75ae45b5ee62fb7417e2c3cf840
Gabriel Santos Farias <gfarias@server01.foursys.local>
2022-06-02 16:42:37 -0300

    Merged

--- app/src/main/java/com/example/appbanco/PixTransfDestino.java
+++ app/src/main/java/com/example/appbanco/PixTransfDestino.java
@@ -2,13 +2,23 @@ package com.example.appbanco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PixTransfDestino extends AppCompatActivity {
 
+    Button btnPixFinal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pix_transf_destino);
+
+        btnPixFinal = findViewById(R.id.btnPixFinal);
+
+        btnPixFinal.setOnClickListener(view -> {
+            startActivity(new Intent(this, PixTransfFinal.class));
+        });
     }
 }
--- app/src/main/java/com/example/appbanco/PixTransfFinal.java
+++ app/src/main/java/com/example/appbanco/PixTransfFinal.java
@@ -2,13 +2,25 @@ package com.example.appbanco;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class PixTransfFinal extends AppCompatActivity {
+
+    Button btnTransf;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pix_transf_final);
+
+        btnTransf = findViewById(R.id.btnTransf);
+
+        btnTransf.setOnClickListener(view -> {
+            startActivity(new Intent(this, Home.class));
+            Toast.makeText(this, "Transferencia realizada.", Toast.LENGTH_SHORT).show();
+        });
     }
 }
