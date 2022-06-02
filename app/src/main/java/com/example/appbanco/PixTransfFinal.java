0e8f699f8ef21f707edc84dad6e58656a84b7b47
Igor Montezuma de Miranda <imiranda@server01.foursys.local>
2022-06-02 12:02:57 -0300

    Tela Traanserência Pix

---
+++ app/src/main/java/com/example/appbanco/PixTransfFinal.java
@@ -0,0 +1,14 @@
+package com.example.appbanco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PixTransfFinal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pix_transf_final);
+    }
+}
