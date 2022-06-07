61cefa41702236beebf309d1dd54850d25cc637c
Igor Montezuma de Miranda <imiranda@server01.foursys.local>
2022-06-07 13:27:19 -0300

    Criação do Package Mascara

--- app/src/main/java/com/example/appbanco/view/Pagamentos/Deposito/DepositofFormActivity.java
+++ app/src/main/java/com/example/appbanco/view/Pagamentos/Deposito/DepositofFormActivity.java
@@ -9,6 +9,6 @@ public class DepositofFormActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_deposito_form);
+        //setContentView(R.layout.activity_deposito_form);
     }
 }
