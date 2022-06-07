e1db8de29ce9048bf173f5f5396d5d0782606ee2
Igor Montezuma de Miranda <imiranda@server01.foursys.local>
2022-06-07 11:48:24 -0300

    Tela DepÃosito Inicial

--- app/src/main/java/com/example/appbanco/view/Pagamentos/Pix/PixTransf.java
+++ app/src/main/java/com/example/appbanco/view/Pagamentos/Pix/PixTransf.java
@@ -4,13 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.appbanco.R;
 
 public class PixTransf extends AppCompatActivity {
 
     Button btnProximo;
+    private EditText edtValorPix;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,4 +28,14 @@ public class PixTransf extends AppCompatActivity {
 
 
     }
+
+    /*public void validaDados(View view){
+        double valor = (double) edtValorPix.getRawValue / 100;
+
+        if(valor >= 15){
+
+        }else{
+
+        }
+    }*/
 }
