13887bb99a723c2d127da34e1848bd420e24b6f3
Igor Montezuma de Miranda <imiranda@server01.foursys.local>
2022-06-07 11:48:24 -0300

    Tela DepÃosito Inicial

---
+++ app/src/main/java/com/example/appbanco/view/Pagamentos/Deposito/DepositofFormActivity.java
@@ -0,0 +1,14 @@
+package com.example.appbanco.view.Pagamentos.Deposito;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DepositofFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_deposito_form);
+    }
+}
