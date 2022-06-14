909ca87db18ed8affba2d0da36915d200a2c963b
Igor Montezuma de Miranda <imiranda@server01.foursys.local>
2022-06-14 15:56:10 -0300

    Correções de Código

--- app/src/main/java/com/example/appbanco/view/Pagamentos/Pix/Chave_Pix/CriaChavePix.java
+++ app/src/main/java/com/example/appbanco/view/Pagamentos/Pix/Chave_Pix/CriaChavePix.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.appbanco.databinding.ActivityCriaChavePixBinding;
+
 public class CriaChavePix extends AppCompatActivity {
 
     private ActivityCriaChavePixBinding binding;
