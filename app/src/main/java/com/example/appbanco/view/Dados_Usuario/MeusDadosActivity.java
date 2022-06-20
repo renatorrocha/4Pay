2c9425423102fa5b4fe010903845d7ee09058477
Igor Montezuma <igorminranda17@gmail.com>
2022-06-19 23:23:36 -0300

    Fragment Meus Dados (deve ser corrigida)

--- app/src/main/java/com/example/appbanco/view/Dados_Usuario/MeusDadosActivity.java
+++ app/src/main/java/com/example/appbanco/view/Dados_Usuario/MeusDadosActivity.java
@@ -2,15 +2,45 @@ package com.example.appbanco.view.Dados_Usuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.appbanco.R;
+import com.example.appbanco.view.Home.Home;
 
 public class MeusDadosActivity extends AppCompatActivity {
+
+    private ImageView ivArrowBack;
+    private ImageView ivEditar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meus_dados);
+
+        iniciaComponentes();
     }
+
+    private void iniciaComponentes() {
+
+        ivArrowBack.findViewById(R.id.ivArrowBack);
+        ivEditar.findViewById(R.id.ivArrowBack);
+
+        ivArrowBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MeusDadosActivity.this, Home.class));
+            }
+        });
+
+        ivEditar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MeusDadosActivity.this, AtualizarDadosActivity.class));
+            }
+        });
+    }
+
 }
