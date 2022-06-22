556af053a59972aa5aeee9b6958ee91742c71cad
Gabriel Santos Farias <gfarias@server01.foursys.local>
2022-06-22 14:34:40 -0300

    merge

---
+++ app/src/main/java/com/example/appbanco/view/Pagamentos/Pix/Chave_Pix/RegistrarChavePix.java
@@ -0,0 +1,16 @@
+package com.example.appbanco.view.Pagamentos.Pix.Chave_Pix;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appbanco.R;
+
+public class RegistrarChavePix extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registrar_chave_pix);
+    }
+}
