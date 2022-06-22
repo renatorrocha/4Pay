d046fd9b2b91fc81826705b8265d971e18996105
Igor Montezuma de Miranda <imiranda@server01.foursys.local>
2022-06-22 14:04:12 -0300

    Inserindo foto de usuário e recuperando do Firebase

--- app/src/main/java/com/example/appbanco/help/FirebaseHelper.java
+++ app/src/main/java/com/example/appbanco/help/FirebaseHelper.java
@@ -3,14 +3,24 @@ package com.example.appbanco.help;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class FirebaseHelper {
 
     private static FirebaseAuth auth=null;
     private static DatabaseReference databaseReference;
+    private static StorageReference storageReference;
 
     public static String getIdFirebase(){
         return getAuth().getUid();
+    }
+
+    public static StorageReference getStorageReference(){
+        if(storageReference == null){
+            storageReference = FirebaseStorage.getInstance().getReference();
+        }
+        return  storageReference;
     }
 
     public static DatabaseReference getDatabaseReference(){
