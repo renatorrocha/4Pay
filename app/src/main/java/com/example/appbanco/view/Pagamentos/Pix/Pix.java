06199186364b83035a11d8692f54b59369a0d91a
Gabriel Santos Farias <gfarias@server01.foursys.local>
2022-06-29 12:25:25 -0300

    Criar qrCode

--- app/src/main/java/com/example/appbanco/view/Pagamentos/Pix/Pix.java
+++ app/src/main/java/com/example/appbanco/view/Pagamentos/Pix/Pix.java
@@ -1,10 +1,15 @@
 package com.example.appbanco.view.Pagamentos.Pix;
 
+import static android.graphics.Color.BLACK;
+import static android.graphics.Color.WHITE;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,11 +19,16 @@ import com.example.appbanco.view.Home.Home;
 import com.example.appbanco.view.Pagamentos.Pix.Chave_Pix.TipoChave;
 import com.example.appbanco.view.Pagamentos.Pix.PixCobrar.PixCobrar;
 import com.example.appbanco.view.Pagamentos.Pix.PixTransferir.PixTransf;
+import com.example.appbanco.view.Pagamentos.Pix.QRCode.CriarPixQrCode;
 import com.example.appbanco.view.Pagamentos.Pix.QRCode.PixQrCode;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
 
 public class Pix extends AppCompatActivity {
 
-    private TextView tvEnviarPix, tvDepositarPix, tvLerQrCode;
+    private TextView tvEnviarPix, tvDepositarPix, tvLerQrCode, tvEnviarQrCode;
     private ConstraintLayout confPix;
     private ImageView ivArrowBack;
 
@@ -34,6 +44,7 @@ public class Pix extends AppCompatActivity {
         confPix = findViewById(R.id.confPix);
         ivArrowBack = findViewById(R.id.ivArrowBack);
         tvLerQrCode = findViewById(R.id.tvLerQrCode);
+        tvEnviarQrCode = findViewById(R.id.tvEnviarQrCode);
 
         ivArrowBack.setOnClickListener(view1 -> {
             startActivity(new Intent(this, Home.class));
@@ -66,5 +77,15 @@ public class Pix extends AppCompatActivity {
         tvLerQrCode.setOnClickListener(v -> {
             startActivity(new Intent(Pix.this, PixQrCode.class));
         });
+
+        tvEnviarQrCode.setOnClickListener(v -> {
+            startActivity(new Intent(Pix.this, CriarPixQrCode.class));
+
+        });
+
     }
+
+
+
+
 }
